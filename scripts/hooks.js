6403e426bef12af414fc844bb04cab776b027265
huayun321 <huayun321@gmail.com>
2015-10-03 23:28:07 +0800

    测试 git ssh key 验证

--- scripts/hooks.js
+++ scripts/hooks.js
@@ -18,7 +18,7 @@ handler.on('push', function (event) {
         return;
     }
     console.log('Received a push on production, build started...');
-    exec('./scripts/deploy.sh', function(error, stdout, stderr) {
+    exec('./deploy.sh', function(error, stdout, stderr) {
         console.log(stdout);
         if(error != null) {
             console.log('error: ', error);
